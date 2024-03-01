151cd70d670d7be0f182c97ac78696970510cfb9
Rbc6 <91257696+Rbc6@users.noreply.github.com>
2024-03-01 11:16:41 +0100

    Delete PFE directory

--- PFE/Back_End/pfe/src/main/java/com/pfe/Repositories/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pfe.Repositories;
-
-import com.pfe.entites.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-
-}
--- PFE/Back_End/pfe/src/main/java/com/pfe/services/user/UserService.java
+++
@@ -1,5 +0,0 @@
-package com.pfe.services.user;
-
-public interface UserService {
-    
-}
